9097f2d89c9419fdc35efed93f3e0528d404be59
LiasCode <liasraul00@gmail.com>
2023-02-03 13:03:46 -0500

    REMOVE: innecesary static function from router in cristalyx module. ADD : login page in test server implementation

---
+++ test/server.js
@@ -0,0 +1,24 @@
+const path = require("path");
+const { cristalyx , staticMiddleware} = require("../lib");
+
+const PORT = 8080;
+
+const AppServer = cristalyx(__dirname);
+
+AppServer.get("/", (_req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+AppServer.get("/login", (_req, res) => {
+  res.sendFile(path.join(__dirname, "public", "login.html"));
+});
+
+// statics in route "/public"
+AppServer.get(
+  (url = "") => url.startsWith("/public"),
+  staticMiddleware("public")
+);
+
+AppServer.listen(PORT, () => {
+  console.log("SERVER RUNNING IN: " + "http://localhost:" + PORT);
+});
