9b7a891a45b1bde60cca8530254d7b4655d746d9
LiasCode <liasraul00@gmail.com>
2024-03-06 01:03:47 -0500

    feat: add custom route matcher handler

--- src/findHandlers.ts
+++ src/findHandlers.ts
@@ -1,11 +1,14 @@
 import { Method, RouteListener, routesStorage } from "./router";
 
-
 export function findHandlers(routePath: string, method: Method): RouteListener[] | null {
   if (!routesStorage[method]) return null;
 
   for (const routeItem of routesStorage[method]) {
-    if (routeItem.route === routePath) return routeItem.listeners;
+    if (typeof routeItem.route === "string") {
+      if (routeItem.route === routePath) return routeItem.listeners;
+    } else if (typeof routeItem.route === "function") {
+      if (routeItem.route(routePath)) return routeItem.listeners;
+    }
   }
   return null;
 }
