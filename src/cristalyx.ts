5a0206187ac6ef7d170679b34e92415503a4280d
LiasCode <liasraul00@gmail.com>
2024-03-06 00:41:09 -0500

    feat: add handler next management and file spliting

--- src/cristalyx.ts
+++ src/cristalyx.ts
@@ -1,67 +1,15 @@
 import http from "node:http";
+import fs from "node:fs";
+import { Method } from "./router";
+import { findHandlers } from "./findHandlers";
+import { execHandlers } from "./execHandlers";
 
-type Request = http.IncomingMessage;
-
-type Response = {
-  status: (statusCode: number) => void;
-  sendFile: (filePath: string) => void;
-  json: (data: any) => void;
-  text: (data: any) => void;
-};
-
-type RouteListener = (req: Request, res: Response) => any;
-
-type Route = { route: string; listeners: RouteListener[] };
-
-type RoutesStorage = {
-  GET: Route[];
-  POST: Route[];
-  PUT: Route[];
-  DELETE: Route[];
-};
-
-const routesStorage: RoutesStorage = {
-  GET: [],
-  POST: [],
-  PUT: [],
-  DELETE: [],
-};
-
-export function Router() {
-  return {
-    get(route: string, ...listeners: RouteListener[]) {
-      routesStorage.GET.push({
-        route,
-        listeners,
-      });
-    },
-    post(route: string, ...listeners: RouteListener[]) {
-      routesStorage.POST.push({
-        route,
-        listeners,
-      });
-    },
-    put(route: string, ...listeners: RouteListener[]) {
-      routesStorage.PUT.push({
-        route,
-        listeners,
-      });
-    },
-    delete(route: string, ...listeners: RouteListener[]) {
-      routesStorage.DELETE.push({
-        route,
-        listeners,
-      });
-    },
-  };
-}
-
-export function cristalyx(httpServerIntance?: http.Server) {
+export function Cristalyx(httpServerIntance?: http.Server) {
   if (!httpServerIntance) {
     httpServerIntance = http.createServer();
   }
 
-  //  Evento REQUEST :
+  //  Evento Request:
   httpServerIntance.on("request", (request, response) => {
     const body: any[] = [];
 
@@ -69,7 +17,22 @@ export function cristalyx(httpServerIntance?: http.Server) {
       .on("data", (chunk) => body.push(chunk))
       .on("end", () => {
         const requestBodyData = Buffer.concat(body).toString("utf-8");
-        response.end("");
+
+        const methodParsed: Method = request.method?.toUpperCase() as Method;
+
+        const handlers = findHandlers(request.url!, methodParsed);
+
+        if (!handlers) {
+          response.statusCode = 404;
+          response.end("Not Found");
+          return;
+        }
+
+        execHandlers(
+          handlers,
+          Object.assign(request, { body: requestBodyData }),
+          parseResponse(response)
+        );
       });
 
     // ## ERRORS
@@ -83,4 +46,24 @@ export function cristalyx(httpServerIntance?: http.Server) {
   });
 
   return httpServerIntance;
+}
+
+export function parseResponse(response: http.ServerResponse) {
+  return Object.assign(response, {
+    status: (statusCode: number) => {
+      response.statusCode = statusCode;
+    },
+    sendFile: (filePath: string) => {
+      response.setHeader("Content-Type", "text/html");
+      fs.createReadStream(filePath).pipe(response);
+    },
+    json: (data: any) => {
+      response.setHeader("Content-Type", "application/json");
+      response.end(JSON.stringify(data));
+    },
+    text: (data: any) => {
+      response.setHeader("Content-Type", "text/plain");
+      response.end(data);
+    },
+  });
 }
